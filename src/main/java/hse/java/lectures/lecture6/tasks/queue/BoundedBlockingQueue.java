d3127e13ee921b051dcaed61397ee9124e2a89cc
Timofeev_V_S <timofeevvseva@gmail.com>
2026-03-22 21:12:17 +0300

    queue:

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -1,25 +1,49 @@
 package hse.java.lectures.lecture6.tasks.queue;
 
-public class BoundedBlockingQueue<T> {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class BoundedBlockingQueue<T> {
+    private final Queue<T> queue = new LinkedList<>();
+    private final int capacity;
 
     public BoundedBlockingQueue(int capacity) {
-
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be positive");
+        }
+        this.capacity = capacity;
     }
 
     public void put(T item) throws InterruptedException {
+        if (item == null) {
+            throw new NullPointerException("Cannot put null into queue");
+        }
 
+        synchronized (queue) {
+            while (queue.size() == capacity) {
+                queue.wait();
+            }
+            queue.add(item);
+            queue.notifyAll();
+        }
     }
 
     public T take() throws InterruptedException {
-        return null;
+        synchronized (queue) {
+            while (queue.isEmpty()) {
+                queue.wait();
+            }
+            T item = queue.poll();
+            queue.notifyAll();
+            return item;
+        }
     }
 
-    public int size() {
-        return 0;
+    public synchronized int size() {
+        return queue.size();
     }
 
     public int capacity() {
-        return 0;
+        return capacity;
     }
 }
